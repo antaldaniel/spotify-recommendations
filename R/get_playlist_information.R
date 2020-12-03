191d07d59fb38485708b2d11ede4e29ed4393658
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-12-03 18:55:21 +0100

    use playlists, too

--- R/get_playlist_information.R
+++ R/get_playlist_information.R
@@ -10,26 +10,35 @@
 #' @return A list of unique tracks and unique artists from a playlist.
 #' @export
 
-get_playlist_information <- function( playlist_id,
+get_playlist_information <- function( playlist_id = NULL,
+                                      playlist = NULL,
                                       token = NULL) {
 
   if (is.null(token)) token <- get_spotify_access_token()
+  if  (!is.null(playlist_id)) {
+
+    possibly_get_playlist <- purrr::possibly(
+      .f = spotifyr::get_playlist, NULL)
+
+    user_playlist <- possibly_get_playlist(playlist_id,
+                                           authorization = token )
+    user_playlist_features <- spotifyr::get_playlist_audio_features(
+      playlist_uris = playlist_id,
+      authorization = token )
+
+  } else {
+    user_playlist <- playlist
+    user_playlist_features <- spotifyr::get_track_audio_features(
+      ids = user_playlist$tracks$items$track.id,
+      authorization = token)
+    user_playlist_features <- bind_cols (user_playlist$tracks$items, user_playlist_features)
+  }
 
   . <- id <- n <- NULL
-
-  possibly_get_playlist <- purrr::possibly(
-    .f = spotifyr::get_playlist, NULL)
-
-  user_playlist <- possibly_get_playlist(playlist_id,
-                                         authorization = token )
 
   if ( is.null(user_playlist) ) {
     warning ("Could not get playlist")
     return(NULL)}
-
-  user_playlist_features <- spotifyr::get_playlist_audio_features(
-    playlist_uris = playlist_id,
-    authorization = token )
 
   user_playlist_features$release_country_code <- get_release_country(
     user_playlist_features$track.external_ids.isrc)
