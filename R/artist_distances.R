d64e3f3077a074cb5e736c731b1f0faf45a90834
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-12-15 15:25:08 +0100

    updated artist distances

--- R/artist_distances.R
+++ R/artist_distances.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset containing related artists.
 #'
-#' @format A data frame with 49094 rows and 4 variables:
+#' @format A data frame with 118981 rows and 4 variables:
 #' \describe{
 #'   \item{spotify_artist_id}{Spotify Artist ID}
 #'   \item{recommendation}{Recommendation based on spotify_artist_id}
--- data-raw/create_artist_distance.R
+++ data-raw/create_artist_distance.R
@@ -2,12 +2,32 @@
 library(tidyverse)
 library(spotifyrecommendations)
 
+spotify_recommendations_dir <- file.path("..", "..", "__SR", "spotify-features", "tables"  )
+
+dir ( spotify_recommendations_dir )
+
+
+
 sk_artist_distance <- read.csv(file.path(
-  'data-raw', 'SK_Artist_Distances.csv'
+  spotify_recommendations_dir, 'SK_Artist_Distances.csv'
 ))  %>%
   setNames ( ., c("recommendation", "spotify_artist_id", "distance")) %>%
   mutate ( national_identity = "sk")
 
-artist_distances <- sk_artist_distance
+hu_artist_distance <- read.csv(file.path(
+  spotify_recommendations_dir, 'HU_Artist_Distances.csv'
+))  %>%
+  setNames ( ., c("recommendation", "spotify_artist_id", "distance")) %>%
+  mutate ( national_identity = "hu")
+
+nl_artist_distance <- read.csv(file.path(
+  spotify_recommendations_dir, 'NL_Artist_Distances.csv'
+))  %>%
+  setNames ( ., c("recommendation", "spotify_artist_id", "distance")) %>%
+  mutate ( national_identity = "nl")
+
+artist_distances <- sk_artist_distance %>%
+  dplyr::bind_rows( hu_artist_distance ) %>%
+  dplyr::bind_rows( nl_artist_distance )
 
 usethis::use_data(artist_distances, overwrite = TRUE)
